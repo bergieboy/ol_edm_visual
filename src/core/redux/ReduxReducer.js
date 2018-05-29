8da40704d50a56524a41ac5ccef29c00e652be65
Todd <toddleebergman@gmail.com>
2018-05-29 13:11:58 -0700

    removed auth dependencies

---
+++ src/core/redux/ReduxReducer.js
@@ -0,0 +1,12 @@
+/*
+ * @flow
+ */
+
+// import { AuthReducer } from 'lattice-auth';
+import { combineReducers } from 'redux-immutable';
+
+export default function reduxReducer() {
+
+  return combineReducers({
+  });
+}
